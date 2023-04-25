9dd09183e3884ed640aa2047bb7ffc79471ca48f
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-04-25 19:46:26 +0100

    Update fine-tune and push.py

--- fine-tune and push.py
+++ fine-tune and push.py
@@ -39,11 +39,11 @@ training_args = TrainingArguments(
     per_device_train_batch_size=4,
     save_total_limit=1,
     logging_steps=100,
-    evaluation_strategy="no",
-    eval_steps=500,
+    evaluation_strategy="no",  # Change this line
     load_best_model_at_end=True,
     metric_for_best_model="loss",
 )
+
 
 # Create a Trainer instance
 trainer = Trainer(
